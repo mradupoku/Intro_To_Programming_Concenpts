5ad9307423a607dcfcb32a85c1a28621231667f3
mradupoku <104675209+mradupoku@users.noreply.github.com>
2026-02-08 23:14:35 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Jeffrey")
+PI = 3.1416
+
+#Functions
+def circle_area(radius):
+    return PI * radius ** 2
+
+
+def total_due(money, tax_rate):
+    return money + (money * tax_rate)
+
+
+def to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+#Main Program
+
+# Area of Circle
+radius = float(input("Enter radius: "))
+area = circle_area(radius)
+print("Area:", format(area, ".2f"))
+
+# Temperature Conversion
+fahrenheit = float(input("Enter Fahrenheit temperature: "))
+celsius = to_celsius(fahrenheit)
+print("Celsius:", format(celsius, ".5f"))
